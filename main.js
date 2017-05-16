b4a66251571a80aec732ca2c3eec8c4ed74f1e92
Alicia Riggs <aliciariggs.15.ar@gmail.com>
2017-05-16 09:52:45 -0700

    update

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var answer1 = document.querySelector("#answer1")
 
 var sum = items.reduce(function(a,b){
 	return a + b.price
-},0)
+},0) // why is it not a.price? 
 
 var avg = sum / items.length
-answer1.innerHTML = 'The average price is $${avg.toFixed(2)}.'
+answer1.innerHTML = `The average price is $${avg.toFixed(2)}.`
 //
 
 var answer2 = document.querySelector("#answer2") 
@@ -14,16 +14,23 @@ var answer2 = document.querySelector("#answer2")
 var priceRange = items.filter(function(item){
 	return item.price > 14 && item.price < 18
 })
+
+var answer2.html = ''
+priceRange.forEach(function(value){
+	answer2html += value.title + "\n"
+})
+
+answer2.innerHTML = answer2html
 //
 
-var gbp = items.filter(functoin(item){
+var gbp = items.filter(function(item){
 	return item.currency_code === "GBP"
 })
 
 var answer3 = document.querySelector("#answer3")
 var answer3html = ""
-gbp.forEach(functoin(item){
-	answer3html += '${item.title} costs &pound; ${item.price}\n'
+gbp.forEach(function(item){
+	answer3html += `${item.title} costs &pound;${item.price}\n`
 })
 
-answer3.innerHTML =answer3html
+answer3.innerHTML = answer3html
